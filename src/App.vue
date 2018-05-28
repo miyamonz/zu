45aecf3738e62b39497e9e68b3fc32d192072049
Koichi Miyamoto <m5813k@gmail.com>
2018-05-29 02:15:22 +0900

    とりあえず

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,50 @@
 <template>
-  <div id="app">
+  <div id="app" @mousemove="mousemove">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <textarea id="" name="" cols="30" rows="10"></textarea>
+    <textarea 
+        style="position: relative " 
+        :style="textPos" 
+        v-model="text"
+        @keyup="key"
+    />
+    {{$store.state.words}}
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    mousemove(e) {
+      this.x = e.clientX;
+      this.y = e.clientY;
+    },
+    clear() {
+      this.$nextTick(() => (this.text = ""));
+    },
+    key(e) {
+      if (e.key == "Enter") {
+        if (this.text !== "") this.$store.commit("add", this.text);
+        this.clear();
+      }
+    }
+  },
+  computed: {
+    textPos() {
+      return {
+        left: this.x + "px",
+        top: this.y + "px"
+      };
+    }
+  },
+  data() {
+    return { x: 0, y: 0, text: "" };
+  }
+};
+</script>
 
 <style>
 #app {
