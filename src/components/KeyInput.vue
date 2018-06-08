ccbc9bb5d5fad780f6f3d3b71240c50dc5e70051
Koichi Miyamoto <m5813k@gmail.com>
2018-06-09 00:19:25 +0900

    key event ただした

--- src/components/KeyInput.vue
+++ src/components/KeyInput.vue
@@ -4,25 +4,19 @@
           style="position: relative " 
           :style="textPos" 
           v-model="text"
-          @keyup="keyup"
-          @keydown="keydown"
+          @keypress.prevent.enter="send"
       />
     </div>
 </template>
 <script>
 export default {
   methods: {
-    clear() {
+    send() {
+      if (this.text === "") return;
+
+      this.$emit("enter", this.text);
       this.text = "";
-    },
-    keyup(e) {
-      if (e.key == "Enter") {
-        this.text = this.text.slice(0, -1);
-        if (this.text !== "") this.$emit("enter", this.text);
-        this.clear();
-      }
-    },
-    keydown() {}
+    }
   },
   computed: {
     textPos() {
